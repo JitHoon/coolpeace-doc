483c8acf40e9533e5cb53c1d7071f1afca133010
JitHoon <imessi0336@gmail.com>
2024-01-11 14:01:54 +0900

    fix: 첫 화면 로딩 에러

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,6 @@
 import React from "react";
 import { Redirect } from "@docusaurus/router";
 
-import styles from "./styles.module.css";
-import clsx from "clsx";
-
 export default function Home() {
-  return (
-    <main className={clsx("hero hero--primary", styles.heroBanner)}>
-      <Redirect to="/docs/guide/dashboad" />;
-    </main>
-  );
+  return <Redirect to="/docs/guide/dashboad" />;
 }
